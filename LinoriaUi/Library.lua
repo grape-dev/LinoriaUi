af82aa51f9c733cb64706cc7adb4e78cb2c2ce00
lil uzi squirt <qripxd@gmail.com>
2024-09-26 10:19:27 -0400

    Update Library.lua

--- LinoriaUi/Library.lua
+++ LinoriaUi/Library.lua
@@ -3017,7 +3017,7 @@ do
 
 	local WatermarkOuter = Library:Create('Frame', {
 		BorderColor3 = Color3.new(0, 0, 0);
-		Position = UDim2.new(0, 830, 0, -25);
+		Position = UDim2.new(0, 250, 0, -25);
 		Size = UDim2.new(0, 213, 0, 20);
 		ZIndex = 200;
 		Visible = false;
